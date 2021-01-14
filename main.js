26eb6b8991c2ecc7c21bcccc73092d5da8827ee3
2san <chillout2san@gmai.com>
2021-01-14 20:53:37 +0900

    js3-1質問用②

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const newTodo = document.createElement('tr');
-const addElement = [
+// 各種DOM取得
+const textboxDOM = document.querySelector("#textbox");
+const btnDOM = document.querySelector("#btn");
+const tbodyDOM = document.querySelector('#tbody');
+
+//追加ボタン押下時の動作
+btnDOM.addEventListener('click', function(){
+  const newTodo = document.createElement('tr');
+  const elementArray = [
   '<td></td>',
   '<td></td>', 
   '<td><button>作業中</button></td>',
   '<td><button>削除</button></td>'];
-
-for(let i = 0; i < addElement.length; i++){
-  newTodo.appendChild(document.createTextNode(addElement[i]));
-};
-
-const tbodyDOM = document.querySelector('#tbody')
-tbodyDOM.innerHTML = newTodo;
-
+  elementArray[0] = "0";
+  elementArray[1] = textboxDOM.value;
+  newTodo.innerHTML = elementArray[0] + elementArray[1] + elementArray[2] + elementArray[3];
+  tbodyDOM.appendChild(newTodo);
+})
