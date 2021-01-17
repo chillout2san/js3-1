859d27ddaf2649ec3fb07e60ec859138ec47ed19
2san <chillout2san@gmai.com>
2021-01-17 19:38:48 +0900

    js3-1提出

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 'use strict';
-
-// 各種DOM取得
-const textboxDOM = document.querySelector("#textbox");
-const btnDOM = document.querySelector("#btn");
+// 各種DOMを取得
+const btnDOM = document.querySelector('#btn');
+const textboxDOM = document.querySelector('#textbox');
 const tbodyDOM = document.querySelector('#tbody');
-
-//追加ボタン押下時の動作
+// タスクを格納する配列を定義
+const todos = [];
+let todo = {};
+// カウンターを定義
+let counter = 0;
+// 配列をHTMLに追加する関数を定義
+const displayTodos = function() {
+  const trElement = document.createElement('tr');
+  // インデックス
+  const tdElement0 = document.createElement('td');
+  tdElement0.innerText = todo.id;
+  // 入力内容
+  const tdElement1 = document.createElement('td');
+  tdElement1.innerText = todo.task;
+  // 状態ボタン
+  const tdElement2 = document.createElement('td');
+  const statusBtnElement = document.createElement('button');
+  statusBtnElement.innerText = todo.status;
+  tdElement2.appendChild(statusBtnElement);
+  // 削除ボタン
+  const tdElement3 = document.createElement('td');
+  const deleteBtnElement = document.createElement('button');
+  deleteBtnElement.innerText = '削除';
+  tdElement3.appendChild(deleteBtnElement);
+  // 配列をHTMLへ追加
+  trElement.appendChild(tdElement0);
+  trElement.appendChild(tdElement1);
+  trElement.appendChild(tdElement2);
+  trElement.appendChild(tdElement3);
+  tbodyDOM.appendChild(trElement);
+};
+// 追加ボタンを押下した時の動作
 btnDOM.addEventListener('click', function(){
-  const newTodo = document.createElement('tr');
-  const elementArray = [
-  '<td></td>',
-  '<td></td>', 
-  '<td><button>作業中</button></td>',
-  '<td><button>削除</button></td>'];
-  elementArray[0] = "0";
-  elementArray[1] = textboxDOM.value;
-  newTodo.innerHTML = elementArray[0] + elementArray[1] + elementArray[2] + elementArray[3];
-  tbodyDOM.appendChild(newTodo);
+  todo = {
+    id: counter,
+    task: textboxDOM.value,
+    status: '作業中'
+  };
+  todos.push(todo);
+  counter++;
+  displayTodos();
 })
+
