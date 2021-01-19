4eb8783b0b15c330c03ae3546ae4b7d0db170ea8
2san <chillout2san@gmai.com>
2021-01-19 21:48:02 +0900

    js3-1再提出

--- main.js
+++ main.js
@@ -1,48 +1,65 @@
 'use strict';
 // 各種DOMを取得
-const btnDOM = document.querySelector('#btn');
-const textboxDOM = document.querySelector('#textbox');
-const tbodyDOM = document.querySelector('#tbody');
+const btnElm = document.querySelector('#btn');
+const textboxElm = document.querySelector('#textbox');
+const tbodyElm = document.querySelector('#tbody');
 // タスクを格納する配列を定義
 const todos = [];
-let todo = {};
+
 // カウンターを定義
 let counter = 0;
-// 配列をHTMLに追加する関数を定義
-const displayTodos = function() {
-  const trElement = document.createElement('tr');
-  // インデックス
-  const tdElement0 = document.createElement('td');
-  tdElement0.innerText = todo.id;
-  // 入力内容
-  const tdElement1 = document.createElement('td');
-  tdElement1.innerText = todo.task;
-  // 状態ボタン
-  const tdElement2 = document.createElement('td');
-  const statusBtnElement = document.createElement('button');
-  statusBtnElement.innerText = todo.status;
-  tdElement2.appendChild(statusBtnElement);
-  // 削除ボタン
-  const tdElement3 = document.createElement('td');
-  const deleteBtnElement = document.createElement('button');
-  deleteBtnElement.innerText = '削除';
-  tdElement3.appendChild(deleteBtnElement);
-  // 配列をHTMLへ追加
-  trElement.appendChild(tdElement0);
-  trElement.appendChild(tdElement1);
-  trElement.appendChild(tdElement2);
-  trElement.appendChild(tdElement3);
-  tbodyDOM.appendChild(trElement);
-};
-// 追加ボタンを押下した時の動作
-btnDOM.addEventListener('click', function(){
-  todo = {
+
+// todoを追加する関数
+function createTask() {
+  const todo = {
     id: counter,
-    task: textboxDOM.value,
+    task: textbox.value,
     status: '作業中'
   };
   todos.push(todo);
+};
+
+// 状態ボタンを生成する関数
+function createStatusBtn(array) {
+  const statusBtn = document.createElement('td');
+  const btn = document.createElement('button');
+  btn.innerText = array.status;
+  statusBtn.appendChild(btn);
+return statusBtn;
+};
+
+// 削除ボタンを生成する関数
+function createDelBtn() {
+const delBtn = document.createElement('td');
+const btn = document.createElement('button');
+btn.innerText = "削除";
+delBtn.appendChild(btn);
+return delBtn;
+};
+
+// todoを表示する関数
+const displayTodos = function(array) {
+  const tr = document.createElement('tr');
+  // インデックス
+  const indexTd = document.createElement('td');
+  indexTd.innerText = array.id;
+  // 入力内容
+  const taskTd = document.createElement('td');
+  taskTd.innerText = array.task;
+  // 配列をHTMLへ追加
+  tr.appendChild(indexTd);
+  tr.appendChild(taskTd);
+  tr.appendChild(createStatusBtn(array));
+  tr.appendChild(createDelBtn());
+  tbodyElm.appendChild(tr);
+};
+
+// 追加ボタンを押下した時の動作
+btnElm.addEventListener('click', function() {
+  createTask();
+  const array = todos.slice(-1)[0];
+  displayTodos(array);
   counter++;
-  displayTodos();
-})
+  textboxElm.value = "";
+}); 
 
